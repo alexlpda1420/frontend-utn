2e90bd5e533236487f9c980963da466a8a990f62
Alexis <a.eroldan@hotmail.com>
2025-11-25 18:25:04 -0300

    add: proteccion de rutas

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,14 +5,21 @@ import AboutUs from "../pages/AboutUs";
 import AddProduct from "../pages/AddProduct";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import ProtectedRoute from "../components/ProtectedRoute";
 
- const RouterApp = () => {
+const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre-nosotros" element={<AboutUs />} />
-        <Route path="/agregar-producto" element={<AddProduct />} />
+        <Route
+          path="/agregar-producto"
+          element={
+            <ProtectedRoute>
+              <AddProduct />
+            </ProtectedRoute>}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
       </Routes>
